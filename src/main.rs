7392c2d9cb0bfccb2c1c1c76da3f264a6d3e4394
Avi Weinstock <aweinstock314@gmail.com>
2023-08-11 18:37:39 -0400

    Basic blit example.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::borrow::Cow;
 use wgpu_example::framework::Spawner;
 
+pub const SPRITESHEET_RESOLUTION: u32 = 256;
+
 pub const VERTEX_LAYOUT: wgpu::VertexBufferLayout = wgpu::VertexBufferLayout {
     array_stride: 0,
     step_mode: wgpu::VertexStepMode::Vertex,
@@ -8,8 +10,12 @@ pub const VERTEX_LAYOUT: wgpu::VertexBufferLayout = wgpu::VertexBufferLayout {
 };
 
 struct Example {
-    shape_pipeline: wgpu::RenderPipeline,
+    quad_pipeline: wgpu::RenderPipeline,
+    blit_pipeline: wgpu::RenderPipeline,
+    blit_bind_group: wgpu::BindGroup,
+    spritesheet_view: wgpu::TextureView,
     vertex_buffer: wgpu::Buffer,
+    counter: u32,
 }
 
 impl wgpu_example::framework::Example for Example {
@@ -24,21 +30,21 @@ impl wgpu_example::framework::Example for Example {
             size: 0,
             usage: wgpu::BufferUsages::VERTEX,
             mapped_at_creation: false,
-        }); 
+        });
 
         let module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("shader_module"),
             source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shaders.wgsl"))),
         });
 
-        let shape_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-            label: Some("shape_pipeline_layout"),
+        let quad_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("quad_pipeline_layout"),
             bind_group_layouts: &[],
             push_constant_ranges: &[],
         });
-        let shape_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("shape_pipeline"),
-            layout: Some(&shape_pipeline_layout),
+        let quad_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("quad_pipeline"),
+            layout: Some(&quad_pipeline_layout),
             vertex: wgpu::VertexState {
                 module: &module,
                 entry_point: &"vert_main",
@@ -58,10 +64,81 @@ impl wgpu_example::framework::Example for Example {
             }),
             multiview: None,
         });
+        let blit_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("blit_bind_group_layout"),
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                }],
+            });
+
+        let blit_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("blit_pipeline_layout"),
+            bind_group_layouts: &[&blit_bind_group_layout],
+            push_constant_ranges: &[],
+        });
+        let blit_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("blit_pipeline"),
+            layout: Some(&blit_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &module,
+                entry_point: &"blit_vert_main",
+                buffers: &[VERTEX_LAYOUT.clone()],
+            },
+            primitive: wgpu::PrimitiveState::default(),
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+            fragment: Some(wgpu::FragmentState {
+                module: &module,
+                entry_point: &"blit_frag_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::PREMULTIPLIED_ALPHA_BLENDING),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            multiview: None,
+        });
+
+        let spritesheet = device.create_texture(&wgpu::TextureDescriptor {
+            label: Some("spritesheet"),
+            size: wgpu::Extent3d {
+                width: SPRITESHEET_RESOLUTION,
+                height: SPRITESHEET_RESOLUTION,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: config.format,
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
+            view_formats: &[],
+        });
+        let spritesheet_view = spritesheet.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let blit_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("blit_bind_group"),
+            layout: &blit_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&spritesheet_view),
+            }],
+        });
 
         Example {
-            shape_pipeline,
+            quad_pipeline,
+            blit_pipeline,
+            blit_bind_group,
             vertex_buffer,
+            spritesheet_view,
+            counter: 0,
         }
     }
     fn resize(&mut self, _: &wgpu::SurfaceConfiguration, _: &wgpu::Device, _: &wgpu::Queue) {}
@@ -73,22 +150,50 @@ impl wgpu_example::framework::Example for Example {
         queue: &wgpu::Queue,
         _: &Spawner<'_>,
     ) {
-        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: Some("frame_encoder") });
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("frame_encoder"),
+        });
         {
+            let load = if self.counter == 0 {
+                wgpu::LoadOp::Clear(wgpu::Color::TRANSPARENT)
+            } else {
+                wgpu::LoadOp::Load
+            };
             let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("shapes_rpass"),
+                label: Some("quad_rpass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: surface,
-                    resolve_target: None, 
-                    ops: wgpu::Operations { load: wgpu::LoadOp::Clear(wgpu::Color::WHITE), store: true },
+                    view: &self.spritesheet_view,
+                    resolve_target: None,
+                    ops: wgpu::Operations { load, store: true },
                 })],
                 depth_stencil_attachment: None,
                 ..Default::default()
             });
             rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
 
-            rpass.set_pipeline(&self.shape_pipeline);
-            rpass.draw(0..6, 0..4);
+            rpass.set_pipeline(&self.quad_pipeline);
+            let instance_index = self.counter % 4;
+            rpass.draw(0..6, instance_index..instance_index + 1);
+            self.counter = self.counter.saturating_add(1);
+        }
+        {
+            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("blit_rpass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: surface,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::WHITE),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                ..Default::default()
+            });
+            rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            rpass.set_pipeline(&self.blit_pipeline);
+            rpass.set_bind_group(0, &self.blit_bind_group, &[]);
+            rpass.draw(0..6, 0..1);
         }
         queue.submit(vec![encoder.finish()]);
     }
--- src/shaders.wgsl
+++ src/shaders.wgsl
@@ -21,27 +21,50 @@ const COLORS: array<vec4<f32>, 4> = array<vec4<f32>, 4>(
 	vec4(1.0, 1.0, 0.0, 1.0),
 );
 
-struct FragData {
+struct QuadFragData {
 	@builtin(position) position: vec4<f32>,
 	@location(0) color: vec4<f32>,
 }
 
 @vertex
-fn vert_main(@builtin(vertex_index) vertex_index: u32, @builtin(instance_index) instance_index: u32) -> FragData {
+fn vert_main(@builtin(vertex_index) vertex_index: u32, @builtin(instance_index) instance_index: u32) -> QuadFragData {
     var quad_vertices = QUAD_VERTICES;
     let position = quad_vertices[vertex_index % 6u];
 	var offsets = OFFSETS;
 	let offset = offsets[instance_index % 4u];
 	var colors = COLORS;
 	let color = colors[instance_index % 4u];
-	var frag_data: FragData;
+	var frag_data: QuadFragData;
 	frag_data.position = vec4<f32>(position.xy / 2.0, 0.0, 1.0) + offset / 2.0;
 	frag_data.color = color;
 	return frag_data;
 }
 
 @fragment
-fn frag_main(frag_data: FragData) -> @location(0) vec4<f32> {
+fn frag_main(frag_data: QuadFragData) -> @location(0) vec4<f32> {
 	return frag_data.color;
 }
 
+struct BlitFragData {
+	@builtin(position) position: vec4<f32>,
+	@location(0) uv: vec2<f32>,
+}
+
+@vertex
+fn blit_vert_main(@builtin(vertex_index) vertex_index: u32, @builtin(instance_index) instance_index: u32) -> BlitFragData {
+    var quad_vertices = QUAD_VERTICES;
+    let position = quad_vertices[vertex_index % 6u];
+	var frag_data: BlitFragData;
+	frag_data.position = position;
+	frag_data.uv = (position.xy + 1.0) / 2.0;
+	return frag_data;
+}
+
+@group(0) @binding(0) var blit_source: texture_2d<f32>;
+
+@fragment
+fn blit_frag_main(frag_data: BlitFragData) -> @location(0) vec4<f32> {
+	//let color = vec4<f32>(frag_data.uv.xy, 0.0, 1.0);
+	let color = textureLoad(blit_source, vec2<u32>(frag_data.uv * vec2<f32>(255.0, 255.0)), 0);
+	return color;
+}
